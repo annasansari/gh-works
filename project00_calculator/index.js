a9222b11df0695b5a71131001be1b9392fbab5be
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2024-06-13 15:22:56 +0500

    create a basic calculater and pulish on npm

--- project00_calculator/index.js
+++ project00_calculator/index.js
@@ -1,3 +1,67 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hello world");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import inquirer from 'inquirer';
+const askQuestion = () => __awaiter(void 0, void 0, void 0, function* () {
+    const prompt = yield inquirer.prompt([
+        {
+            type: 'input',
+            name: 'num1',
+            message: 'Enter the first number:',
+            validate: (input) => !isNaN(Number(input)) || 'Please enter a valid number',
+        },
+        {
+            type: 'input',
+            name: 'num2',
+            message: 'Enter second number',
+            validate: (input) => !isNaN(Number(input)) || 'Please enter a valid number'
+        },
+        {
+            type: 'list',
+            name: 'operation',
+            message: 'Please choose an operation',
+            choices: ['add', 'subtract', 'multiply', 'divid']
+        }
+    ]);
+    return {
+        num1: parseFloat(prompt.num1),
+        num2: parseFloat(prompt.num2),
+        operation: prompt.operation
+    };
+});
+const calculate = (num1, num2, operation) => {
+    if (operation === 'add') {
+        return num1 + num2;
+    }
+    else if (operation === 'subtract') {
+        return num1 - num2;
+    }
+    else if (operation === 'multiply') {
+        return num1 * num2;
+    }
+    else if (operation === 'divid') {
+        return num1 / num2;
+    }
+    else {
+        return console.log("something went wrong!");
+    }
+};
+const answer = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { num1, num2, operation } = yield askQuestion();
+        const result = calculate(num1, num2, operation);
+        if (result != null || result != undefined) {
+            console.log(`The ${operation}ing of ${num1} and ${num2} is ${result}`);
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+answer();
--- project00_calculator/index.ts
+++ project00_calculator/index.ts
@@ -1,1 +1,69 @@
-console.log("hello world")
+#! usr/bin/env node
+// SHABANG
+
+import inquirer from 'inquirer'
+
+
+const askQuestion = async () => {
+    const prompt = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'num1',
+            message: 'Enter the first number:',
+            validate: (input: string) => !isNaN(Number(input)) || 'Please enter a valid number',
+        },
+        {
+            type: 'input',
+            name: 'num2',
+            message: 'Enter second number',
+            validate: (input: string) => !isNaN(Number(input)) || 'Please enter a valid number'
+        },
+        {
+            type: 'list',
+            name: 'operation',
+            message: 'Please choose an operation',
+            choices: ['add' , 'subtract', 'multiply', 'divid']
+        }
+    ])
+
+    return {
+        num1: parseFloat(prompt.num1),
+        num2: parseFloat(prompt.num2),
+        operation: prompt.operation
+    }
+
+}
+
+const calculate = (num1: number, num2: number, operation: string) => {
+    if (operation === 'add') {
+        return num1 + num2
+    }
+    else if (operation === 'subtract') {
+        return num1 - num2
+    }
+    else if (operation === 'multiply') {
+        return num1 * num2
+    }
+    else if (operation === 'divid') {
+        return num1 / num2
+    }
+    else {
+        return console.log("something went wrong!")
+    }
+}
+
+const answer = async () => {
+    try {
+
+        const { num1, num2, operation } = await askQuestion()
+        const result = calculate(num1, num2, operation)
+        if (result != null || result != undefined) {
+            console.log(`The ${operation}ing of ${num1} and ${num2} is ${result}`)
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+answer()
